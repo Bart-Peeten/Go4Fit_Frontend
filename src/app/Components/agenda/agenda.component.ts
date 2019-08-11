6b6334c46251ca159dfac47a422af5ea3a3c103f
Bart-Peeten <bart.peeten@gmail.com>
2019-08-11 23:01:39 +0200

    remove one training on wednesday && add css to make the grid work on Safari

--- src/app/Components/agenda/agenda.component.ts
+++ src/app/Components/agenda/agenda.component.ts
@@ -4,141 +4,163 @@ import {DateService} from '../../Services/date.service';
 import {AuthService} from '../../Services/auth.service';
 
 @Component({
-  selector: 'app-agenda',
-  templateUrl: './agenda.component.html',
-  styleUrls: ['./agenda.component.css']
+    selector: 'app-agenda',
+    templateUrl: './agenda.component.html',
+    styleUrls: ['./agenda.component.css']
 })
 export class AgendaComponent implements OnInit {
-  private weekNumber: number;
-  private firstDayOfWeekString: string;
-  private dateOfWeek: Date;
-  private lastdayOfWeekString: string;
-  private trainingsDaysList: String[];
-  private trainingsTimes: any[][];
-  private trainingsTypes: any[][];
-  private trainingDaysDatesList: any[];
-  private nextWeek = 1;
-  private nextWeekDays = 7;
-  private isOccupied: boolean;
-  private participantName: String;
-  private reservationDay: String;
-  private reservationTime: string;
-  private reservationDate: string;
-  private numberOfReservations: number[] = [];
-  private htmlIndex: number;
+    private weekNumber: number;
+    private firstDayOfWeekString: string;
+    private dateOfWeek: Date;
+    private lastdayOfWeekString: string;
+    private trainingsDaysList: String[];
+    private trainingsTimes: any[][];
+    private trainingsTypes: any[][];
+    private trainingDaysDatesList: any[];
+    private nextWeek = 1;
+    private nextWeekDays = 7;
+    private isOccupied: boolean;
+    private participantName: String;
+    private reservationDay: String;
+    private reservationTime: string;
+    private reservationDate: string;
+    private numberOfReservations: number[] = [];
+    private htmlIndex = 0;
+    private isReserved: boolean[];
+    private canOpenModalValue = 0;
 
-  constructor(private agendaService: AgendaService,
-              private dateService: DateService,
-              private authService: AuthService) {
-  }
-
-  ngOnInit() {
-    this.currentWeek();
-    this.fetchTrainingsDays();
-    this.getDataOfGivenWeek();
-    this.trainingsTimes = this.agendaService.getTrainingsMoments();
-    this.trainingsTypes = this.agendaService.getTrainingsType();
-  }
-
-  getWeekNumber() {
-    return this.dateService.getWeekNumber();
-  }
-
-  getNextWeekData() {
-    this.weekNumber = this.dateService.getWeekNumberOfNextWeek(this.nextWeek);
-    this.trainingDaysDatesList = this.dateService.getDatesofDaysOfNextWeek(this.nextWeekDays);
-    this.getFirstDayOfNextWeekString();
-    this.getLastDayOfNextWeekString();
-    // this.getNumberOfReservations();
-    console.log('De datums van deze week: ');
-    console.log(this.trainingDaysDatesList);
-
-    // Here data for next weeks will be fetched.
-    this.agendaService.getDataForGivenWeek(this.trainingDaysDatesList);
-    this.getDataOfGivenWeek();
-    this.nextWeek += 1;
-    this.nextWeekDays += 7;
-  }
-
-  private getFirstDayOfWeekString() {
-    this.firstDayOfWeekString = 'Ma ' + this.dateService.getFirstDayOfWeek();
-  }
-
-  private getLastDayOfWeekString() {
-    this.lastdayOfWeekString = 'Zo ' + this.dateService.getLastDayOfWeek();
-  }
-
-  private getFirstDayOfNextWeekString() {
-    this.firstDayOfWeekString = 'Ma ' + this.dateService.getFirstDayOfNextWeek(this.nextWeekDays);
-  }
-
-  private getLastDayOfNextWeekString() {
-    this.lastdayOfWeekString = 'Zo ' + this.dateService.getLastDayOfNextWeek(this.nextWeekDays);
-  }
-
-  private fetchTrainingsDays() {
-    this.trainingsDaysList = this.agendaService.getTrainingsdays();
-  }
-
-  private currentWeek() {
-    this.trainingDaysDatesList = this.dateService.getTrainingsDays();
-    console.log('De datums van deze week: ');
-    console.log(this.trainingDaysDatesList);
-    this.weekNumber = this.getWeekNumber();
-    this.getFirstDayOfWeekString();
-    this.getLastDayOfWeekString();
-    this.getDataOfGivenWeek();
-    // Here data for this week will be fetched.
-
-    this.nextWeek = 1;
-      this.nextWeekDays = 7;
-  }
-
-  private getNumberOfReservations(i: number, x: number) {
-    const free = 10 - this.numberOfReservations[this.getIndex(i, x)];
-    console.log('Het aantal bezoekers is: ' + this.numberOfReservations);
-    const freeString = 'Nog ' + free + ' plaatsen vrij';
-    this.isOccupied = free === 0;
-
-    return free === 0 ? 'VOLZET' : freeString;
-  }
-
-  onTrainingDayClick(trainingDay: String, date: string) {
-    this.reservationDay = trainingDay;
-    this.reservationDate = date;
-  }
-
-  onTrainingMomentClick(trainingMoment) {
-    this.reservationTime = trainingMoment;
-    this.participantName = this.authService.name;
-  }
-
-  confirmReservation() {
-    console.log(this.reservationDate + ' : ' + this.reservationTime + ' : ' + this.participantName);
-    const time = this.reservationTime.substring(0, 2);
-    const formatTime = this.dateService.getFullTime(time);
-    const formatReservationDate = this.dateService.formatDate(this.reservationDate);
-    console.log('Formated date en tijd is: ' + formatTime + ' : ' + formatReservationDate);
-    this.agendaService.addReservation(this.participantName,
-      formatReservationDate,
-      formatTime)
-      .subscribe(_ => this.getDataOfGivenWeek());
-  }
-
-  private getDataOfGivenWeek() {
-    this.agendaService.getNumberOfReservationsForGivenWeek(this.trainingDaysDatesList)
-      .subscribe(result => this.numberOfReservations = result);
-  }
-
-  private getIndex(i: number, x: number) {
-    if ((i + x) === 0) {
-      this.htmlIndex = 0;
-      return 0;
-    } else {
-      const indexx = this.htmlIndex + 1;
-      this.htmlIndex += 1;
-
-      return indexx;
+    constructor(private agendaService: AgendaService,
+                private dateService: DateService,
+                private authService: AuthService) {
     }
-  }
+
+    ngOnInit() {
+        this.currentWeek();
+        this.fetchTrainingsDays();
+        this.trainingsTimes = this.agendaService.getTrainingsMoments();
+        this.trainingsTypes = this.agendaService.getTrainingsType();
+    }
+
+    getWeekNumber() {
+        return this.dateService.getWeekNumber();
+    }
+
+    private currentWeek() {
+        this.trainingDaysDatesList = this.dateService.getTrainingsDays();
+        console.log('De datums van deze week: ');
+        console.log(this.trainingDaysDatesList);
+        this.weekNumber = this.getWeekNumber();
+        this.getFirstDayOfWeekString();
+        this.getLastDayOfWeekString();
+        this.getDataOfGivenWeek();
+        // Here data for this week will be fetched.
+
+        this.nextWeek = 1;
+        this.nextWeekDays = 7;
+    }
+
+    getNextWeekData() {
+        this.weekNumber = this.dateService.getWeekNumberOfNextWeek(this.nextWeek);
+        this.trainingDaysDatesList = this.dateService.getDatesofDaysOfNextWeek(this.nextWeekDays);
+        this.getFirstDayOfNextWeekString();
+        this.getLastDayOfNextWeekString();
+        // this.getNumberOfReservations();
+        console.log('De datums van deze week: ');
+        console.log(this.trainingDaysDatesList);
+
+        // Here data for next weeks will be fetched.
+        this.agendaService.getDataForGivenWeek(this.trainingDaysDatesList);
+        this.getDataOfGivenWeek();
+        this.nextWeek += 1;
+        this.nextWeekDays += 7;
+    }
+
+    private getFirstDayOfWeekString() {
+        this.firstDayOfWeekString = 'Ma ' + this.dateService.getFirstDayOfWeek();
+    }
+
+    private getLastDayOfWeekString() {
+        this.lastdayOfWeekString = 'Zo ' + this.dateService.getLastDayOfWeek();
+    }
+
+    private getFirstDayOfNextWeekString() {
+        this.firstDayOfWeekString = 'Ma ' + this.dateService.getFirstDayOfNextWeek(this.nextWeekDays);
+    }
+
+    private getLastDayOfNextWeekString() {
+        this.lastdayOfWeekString = 'Zo ' + this.dateService.getLastDayOfNextWeek(this.nextWeekDays);
+    }
+
+    private fetchTrainingsDays() {
+        this.trainingsDaysList = this.agendaService.getTrainingsdays();
+    }
+
+    private getNumberOfReservations(i: number, x: number) {
+        let index = this.getIndex(i, x);
+        console.log('De lijst isReserved is: ' + this.isReserved);
+        if (this.isReserved[index] === false) {
+            const free = 10 - this.numberOfReservations[index];
+            console.log('Het aantal bezoekers is: ' + this.numberOfReservations);
+            const freeString = 'Nog ' + free + ' plaatsen vrij';
+            this.isOccupied = free === 0;
+            this.canOpenModalValue = 0;
+
+            return free === 0 ? 'VOLZET' : freeString;
+        } else {
+            this.canOpenModalValue = 1;
+            return 'Je bent ingeschreven.';
+        }
+    }
+
+    onTrainingDayClick(trainingDay: String, date: string) {
+        this.reservationDay = trainingDay;
+        this.reservationDate = date;
+    }
+
+    onTrainingMomentClick(trainingMoment) {
+        this.reservationTime = trainingMoment;
+        this.participantName = this.authService.name;
+    }
+
+    confirmReservation() {
+        console.log(this.reservationDate + ' : ' + this.reservationTime + ' : ' + this.participantName);
+        const time = this.reservationTime.substring(0, 2);
+        const formatTime = this.dateService.getFullTime(time);
+        const formatReservationDate = this.dateService.formatDate(this.reservationDate);
+        console.log('Formated date en tijd is: ' + formatTime + ' : ' + formatReservationDate);
+        this.agendaService.addReservation(this.participantName,
+            formatReservationDate,
+            formatTime)
+            .subscribe(_ => this.getDataOfGivenWeek());
+    }
+
+    private getDataOfGivenWeek() {
+        this.agendaService.getNumberOfReservationsForGivenWeek(this.trainingDaysDatesList)
+            .subscribe(result => this.numberOfReservations = result);
+        this.agendaService.getIsParticipantReservedForGivenWeek(this.trainingDaysDatesList)
+            .subscribe(isReserved => this.isReserved = isReserved);
+    }
+
+    private getIndex(i: number, x: number) {
+        console.log('Het inkomende nummer i is: ' + i);
+        console.log('Het inkomende nummer x is: ' + x);
+        console.log('this.htmlIndex is: ' + this.htmlIndex);
+        if ((i + x) === 0) {
+            this.htmlIndex = 0;
+            return 0;
+        } else {
+            const indexx = this.htmlIndex + 1;
+            this.htmlIndex += 1;
+
+            return indexx;
+        }
+    }
+
+    private canOpenModal(): boolean {
+        if (this.canOpenModalValue === 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- src/app/Services/agenda.service.ts
+++ src/app/Services/agenda.service.ts
@@ -7,162 +7,182 @@ import {User} from '../Domains/user.model';
 import {DateService} from './date.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AgendaService {
-  private url: String = 'http://localhost:8080/api/';
-  private trainingDays: Array<String> = ['Dinsdag', 'Woensdag', 'Donderdag', 'Zondag'];
-  private trainingsType: any[][] = [['circuit training', 'circuit training'],
-    ['circuit training', 'circuit training', 'circuit training'],
-    ['cross training'],
-    ['circuit training', 'circuit training', 'circuit training']];
-  private trainingsMoments: any[][] = [['19h - 20h', '20h - 21h'],
-    ['9h - 10h', '19h - 20h', '20h - 21h'],
-    ['19h - 20h'],
-    ['8h - 9h', '9h - 10h', '10h - 11h']];
+    private CIRCUITTRAINING = 'circuittraining';
+    private CROSSTRAINING = 'crosstraining';
+    private url: String = 'http://localhost:8080/api/';
+    private trainingDays: Array<String> = ['Dinsdag', 'Woensdag', 'Donderdag', 'Zondag'];
+    private trainingsType: any[][] = [[this.CIRCUITTRAINING, this.CIRCUITTRAINING],
+        [this.CIRCUITTRAINING, this.CIRCUITTRAINING, this.CIRCUITTRAINING],
+        [this.CROSSTRAINING],
+        [this.CIRCUITTRAINING, this.CIRCUITTRAINING, this.CIRCUITTRAINING]];
+    private trainingsMoments: any[][] = [['19h - 20h', '20h - 21h'],
+        ['9h - 10h', '19h - 20h', '20h - 21h'],
+        ['19h - 20h'],
+        ['8h - 9h', '9h - 10h', '10h - 11h']];
 
-  constructor(private http: HttpClient,
-              private authService: AuthService,
-              private dateService: DateService) {
-  }
-
-  getTrainingsMoments() {
-    return this.trainingsMoments;
-  }
-
-  getTrainingsdays() {
-    return this.trainingDays;
-  }
-
-  getTrainingsType() {
-    return this.trainingsType;
-  }
-
-  getHeaders() {
-    const securityToken = this.authService.getUserName() + ':' + this.authService.getPassword();
-    console.log('Security token is: ' + securityToken);
-    const headers = new HttpHeaders().set('Authorization', 'Basic ' +
-      btoa(securityToken));
-
-    return headers;
-  }
-
-  getParticipants(date: string, time: string) {
-    const params = new HttpParams()
-      .set('date', date)
-      .set('time', time);
-
-    return this.http.get<Participant[]>(this.url + 'reservation/names', {headers: this.getHeaders(), params: params});
-  }
-
-  addReservation(participantName: String, reservationDate: string, reservationTime: String) {
-    const loggedInUser: User[] = [];
-    loggedInUser.push(this.authService.loggedInUser);
-    const reservation = new Reservation(loggedInUser, reservationDate, reservationTime);
-
-    return this.http.post<Reservation>(this.url + 'reservation/', reservation, {headers: this.getHeaders()});
-  }
-
-  addReservationWithOnlyFullName(firstName: string, lastName: string, reservationDate: string, reservationTime: string) {
-    const formatDate = this.dateService.formatDate(reservationDate);
-    const formatTime = this.dateService.getFullTime(reservationTime);
-
-    const params = new HttpParams().set('firstname', firstName)
-      .set('lastname', lastName)
-      .set('date', formatDate)
-      .set('time', formatTime);
-
-    console.log(this.url + 'reservation/onlyname' + params);
-    return this.http.post<Reservation>(this.url + 'reservation/onlyname', params, {headers: this.getHeaders()});
-  }
-
-  removeReservation(firstName: string, lastName: string, reservationDate: string, reservationTime: string) {
-    const formatDate = this.dateService.formatDate(reservationDate);
-    const formatTime = this.dateService.getFullTime(reservationTime);
-    const options = {
-      headers: this.getHeaders(),
-      body: null,
-      params: new HttpParams()
-        .set('firstname', firstName)
-        .set('lastname', lastName)
-        .set('date', formatDate)
-        .set('time', formatTime)
-    };
-    console.log(this.url + 'reservation/delete' + options);
-    return this.http.delete(this.url + 'reservation/delete', options);
-  }
-
-  getDataForGivenWeek(trainingDaysDatesList: any[]) {
-    let formatDatesList: any[] = [];
-    formatDatesList = this.formatDates(trainingDaysDatesList);
-    console.log('De lijst die naar de API wordt gestuurd is: ' + formatDatesList);
-
-    const params = new HttpParams()
-      .append('datesOfWeek', formatDatesList[0])
-      .append('datesOfWeek', formatDatesList[1])
-      .append('datesOfWeek', formatDatesList[2])
-      .append('datesOfWeek', formatDatesList[3]);
-
-    console.log('De params voor de GET requst: ' + params);
-
-    return this.http.get<String[][]>(this.url + 'reservation/weekusers', {params: params});
-  }
-
-  getNumberOfReservationsForGivenWeek(trainingDaysDatesList: any[]) {
-    let formatDatesList: any[] = [];
-    formatDatesList = this.formatDates(trainingDaysDatesList);
-    console.log('De lijst die naar de API wordt gestuurd is: ' + formatDatesList);
-
-    const params = new HttpParams()
-      .append('datesOfWeek', formatDatesList[0])
-      .append('datesOfWeek', formatDatesList[1])
-      .append('datesOfWeek', formatDatesList[2])
-      .append('datesOfWeek', formatDatesList[3]);
-
-    console.log('De params voor de GET requst: ' + params);
-
-    return this.http.get<number[]>(this.url + 'reservation/weekreservaties', {params: params});
-  }
-
-  getNumberOfReservations(date: string, time: string) {
-    const headers = new HttpHeaders({
-      Authorization: 'Basic' +
-        btoa(this.authService.getPassword() + ':' + this.authService.name)
-    });
-
-    const params = new HttpParams()
-      .set('date', date)
-      .set('time', time);
-
-    const result = this.http.get(this.url + 'numberofreservations', {headers: headers, params: params});
-
-    return result;
-  }
-
-  private formatDates(trainingDaysDatesList: any[]) {
-    const formatDatesList: any[] = [];
-    for (const item of trainingDaysDatesList) {
-      const formatDate = this.dateService.formatDate(item);
-      formatDatesList.push(formatDate);
+    constructor(private http: HttpClient,
+                private authService: AuthService,
+                private dateService: DateService) {
     }
-    return formatDatesList;
-  }
 
-  private formatTimes() {
-    let tmpFormatTimesList: Array<string> = [];
-    const formatTimesList: string[][] = [];
-    let i = 0;
-    for (const item of this.trainingsMoments) {
-      for (const time of item) {
-        const result = this.dateService.getFullTime(time);
-        console.log('de geformateerde tijd is: ', result);
-        tmpFormatTimesList.push(result);
-        i++;
-      }
-      formatTimesList.push(tmpFormatTimesList);
-      tmpFormatTimesList = [];
+    getTrainingsMoments() {
+        return this.trainingsMoments;
     }
-    console.log('De geformateerde tijd lijst is: ', formatTimesList);
-    return [];
-  }
+
+    getTrainingsdays() {
+        return this.trainingDays;
+    }
+
+    getTrainingsType() {
+        return this.trainingsType;
+    }
+
+    getHeaders() {
+        const securityToken = this.authService.getUserName() + ':' + this.authService.getPassword();
+        console.log('Security token is: ' + securityToken);
+        const headers = new HttpHeaders().set('Authorization', 'Basic ' +
+            btoa(securityToken));
+
+        return headers;
+    }
+
+    getParticipants(date: string, time: string) {
+        const params = new HttpParams()
+            .set('date', date)
+            .set('time', time);
+
+        return this.http.get<Participant[]>(this.url + 'reservation/names', {headers: this.getHeaders(), params: params});
+    }
+
+    addReservation(participantName: String, reservationDate: string, reservationTime: String) {
+        const loggedInUser: User[] = [];
+        loggedInUser.push(this.authService.loggedInUser);
+        const reservation = new Reservation(loggedInUser, reservationDate, reservationTime);
+
+        return this.http.post<Reservation>(this.url + 'reservation/', reservation, {headers: this.getHeaders()});
+    }
+
+    addReservationWithOnlyFullName(firstName: string, lastName: string, reservationDate: string, reservationTime: string) {
+        const formatDate = this.dateService.formatDate(reservationDate);
+        const formatTime = this.dateService.getFullTime(reservationTime);
+
+        const params = new HttpParams().set('firstname', firstName)
+            .set('lastname', lastName)
+            .set('date', formatDate)
+            .set('time', formatTime);
+
+        console.log(this.url + 'reservation/onlyname' + params);
+        return this.http.post<Reservation>(this.url + 'reservation/onlyname', params, {headers: this.getHeaders()});
+    }
+
+    removeReservation(firstName: string, lastName: string, reservationDate: string, reservationTime: string) {
+        const formatDate = this.dateService.formatDate(reservationDate);
+        const formatTime = this.dateService.getFullTime(reservationTime);
+        const options = {
+            headers: this.getHeaders(),
+            body: null,
+            params: new HttpParams()
+                .set('firstname', firstName)
+                .set('lastname', lastName)
+                .set('date', formatDate)
+                .set('time', formatTime)
+        };
+        console.log(this.url + 'reservation/delete' + options);
+        return this.http.delete(this.url + 'reservation/delete', options);
+    }
+
+    getDataForGivenWeek(trainingDaysDatesList: any[]) {
+        let formatDatesList: any[] = [];
+        formatDatesList = this.formatDates(trainingDaysDatesList);
+        console.log('De lijst die naar de API wordt gestuurd is: ' + formatDatesList);
+
+        const params = new HttpParams()
+            .append('datesOfWeek', formatDatesList[0])
+            .append('datesOfWeek', formatDatesList[1])
+            .append('datesOfWeek', formatDatesList[2])
+            .append('datesOfWeek', formatDatesList[3]);
+
+        console.log('De params voor de GET requst: ' + params);
+
+        return this.http.get<String[][]>(this.url + 'reservation/weekusers', {params: params});
+    }
+
+    getNumberOfReservationsForGivenWeek(trainingDaysDatesList: any[]) {
+        let formatDatesList: any[] = [];
+        formatDatesList = this.formatDates(trainingDaysDatesList);
+        console.log('De lijst die naar de API wordt gestuurd is: ' + formatDatesList);
+
+        const params = new HttpParams()
+            .append('datesOfWeek', formatDatesList[0])
+            .append('datesOfWeek', formatDatesList[1])
+            .append('datesOfWeek', formatDatesList[2])
+            .append('datesOfWeek', formatDatesList[3]);
+
+        console.log('De params voor de GET requst: ' + params);
+
+        return this.http.get<number[]>(this.url + 'reservation/weekreservaties', {params: params});
+    }
+
+
+    getIsParticipantReservedForGivenWeek(trainingDaysDatesList: any[]) {
+        let formatDatesList: any[] = [];
+        formatDatesList = this.formatDates(trainingDaysDatesList);
+
+        const params = new HttpParams()
+            .set('firstname', this.authService.firstname.toString())
+            .set('lastname', this.authService.lastname.toString())
+            .append('datesOfWeek', formatDatesList[0])
+            .append('datesOfWeek', formatDatesList[1])
+            .append('datesOfWeek', formatDatesList[2])
+            .append('datesOfWeek', formatDatesList[3]);
+
+        console.log('De params voor de GET requst: ' + params);
+
+        return this.http.get<boolean[]>(this.url + 'reservation/isParticipantReserved', {params: params});
+    }
+
+    getNumberOfReservations(date: string, time: string) {
+        const headers = new HttpHeaders({
+            Authorization: 'Basic' +
+                btoa(this.authService.getPassword() + ':' + this.authService.name)
+        });
+
+        const params = new HttpParams()
+            .set('date', date)
+            .set('time', time);
+
+        const result = this.http.get(this.url + 'numberofreservations', {headers: headers, params: params});
+
+        return result;
+    }
+
+    private formatDates(trainingDaysDatesList: any[]) {
+        const formatDatesList: any[] = [];
+        for (const item of trainingDaysDatesList) {
+            const formatDate = this.dateService.formatDate(item);
+            formatDatesList.push(formatDate);
+        }
+        return formatDatesList;
+    }
+
+    private formatTimes() {
+        let tmpFormatTimesList: Array<string> = [];
+        const formatTimesList: string[][] = [];
+        let i = 0;
+        for (const item of this.trainingsMoments) {
+            for (const time of item) {
+                const result = this.dateService.getFullTime(time);
+                console.log('de geformateerde tijd is: ', result);
+                tmpFormatTimesList.push(result);
+                i++;
+            }
+            formatTimesList.push(tmpFormatTimesList);
+            tmpFormatTimesList = [];
+        }
+        console.log('De geformateerde tijd lijst is: ', formatTimesList);
+        return [];
+    }
 }
