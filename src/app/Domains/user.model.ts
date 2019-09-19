6ca441e47fce674ef4c36e57f16978020093dd1d
Bart Peeten <bart.peeten@beeyond.nl>
2019-09-19 16:30:26 +0200

    Add new property in user model

--- src/app/Domains/user.model.ts
+++ src/app/Domains/user.model.ts
@@ -1,36 +1,43 @@
 export class User {
-    firstName: String;
-    lastName: String;
-    email: string;
-    phone: String;
-    password: String;
-    enabled: number;
-    role: String;
+  firstName: String;
+  lastName: String;
+  email: string;
+  phone: String;
+  password: String;
+  enabled: number;
+  role: String;
+  removedReservation: boolean;
 
 
-    constructor(lastname: String, firstname: String, email: string, phone: String, password: String, role?: String) {
-        this.lastName = lastname;
-        this.firstName = firstname;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.enabled = 1;
-        this.role = role;
-    }
+  // tslint:disable-next-line:max-line-length
+  constructor(lastname: String, firstname: String, email: string, phone: String, password: String, role?: String, removedReservation?: boolean) {
+    this.lastName = lastname;
+    this.firstName = firstname;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+    this.enabled = 1;
+    this.role = role;
+    this.removedReservation = removedReservation;
+  }
 
-    getRole() {
-      return this.role;
-    }
+  getRole() {
+    return this.role;
+  }
 
-    getFirstName() {
-      return this.firstName;
-    }
+  getFirstName() {
+    return this.firstName;
+  }
 
-    getLastName() {
-      return this.lastName;
-    }
+  getLastName() {
+    return this.lastName;
+  }
 
-    getEmail() {
-      return this.email;
-    }
+  getEmail() {
+    return this.email;
+  }
+
+  getRemovedReservation() {
+    return this.removedReservation;
+  }
 }
