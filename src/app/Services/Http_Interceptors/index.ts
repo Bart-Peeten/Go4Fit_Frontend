402a2e5a63ff3d8c9a347b34a157a709d5bc1b9b
Bart Peeten <bart.peeten@beeyond.nl>
2020-03-05 16:15:40 +0100

    Add spinner for http requests

--- src/app/Services/Http_Interceptors/index.ts
+++ src/app/Services/Http_Interceptors/index.ts
@@ -1,6 +1,8 @@
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AgendaHttpInterceptorService} from './agenda-http-interceptor.service';
+import {LoadingHttpInterceptorService} from './loading-http-interceptor.service';
 
 export const httpInterceptProviders = [
-  {provide: HTTP_INTERCEPTORS, useClass: AgendaHttpInterceptorService, multi: true}
+  {provide: HTTP_INTERCEPTORS, useClass: AgendaHttpInterceptorService, multi: true},
+  {provide: HTTP_INTERCEPTORS, useClass: LoadingHttpInterceptorService, multi: true}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { AgendaComponent } from './Components/agenda/agenda.component';
-import { HomeComponent } from './Components/home/home.component';
-import { CircuitComponent } from './Components/circuit/circuit.component';
-import { PersonalComponent } from './Components/personal/personal.component';
-import { PricesComponent } from './Components/prices/prices.component';
-import { ContactComponent } from './Components/contact/contact.component';
-import { AppRoutingModule } from './Routing/app-routing.module';
-import { ReservationComponent } from './Components/reservation/reservation.component';
-import { LoginComponent } from './Components/login/login.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './Components/navbar/navbar.component';
+import {AgendaComponent} from './Components/agenda/agenda.component';
+import {HomeComponent} from './Components/home/home.component';
+import {CircuitComponent} from './Components/circuit/circuit.component';
+import {PersonalComponent} from './Components/personal/personal.component';
+import {PricesComponent} from './Components/prices/prices.component';
+import {ContactComponent} from './Components/contact/contact.component';
+import {AppRoutingModule} from './Routing/app-routing.module';
+import {ReservationComponent} from './Components/reservation/reservation.component';
+import {LoginComponent} from './Components/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { AuthService } from './Services/auth.service';
-import { SignUpComponent } from './Components/signUp/sign-up.component';
-import { AdminAgendaComponent } from './Components/admin-agenda/admin-agenda.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SignUpAdminComponent } from './Components/sign-up-admin/sign-up-admin.component';
+import {AuthService} from './Services/auth.service';
+import {SignUpComponent} from './Components/signUp/sign-up.component';
+import {AdminAgendaComponent} from './Components/admin-agenda/admin-agenda.component';
+import {HttpClientModule} from '@angular/common/http';
+import {SignUpAdminComponent} from './Components/sign-up-admin/sign-up-admin.component';
 import {httpInterceptProviders} from './Services/Http_Interceptors';
+import {LoaderComponent} from './Components/shared/loader/loader.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {LoaderService} from './Services/loader-service.service';
 
 @NgModule({
   declarations: [
@@ -34,17 +37,23 @@ import {httpInterceptProviders} from './Services/Http_Interceptors';
     LoginComponent,
     SignUpComponent,
     AdminAgendaComponent,
-    SignUpAdminComponent
+    SignUpAdminComponent,
+    LoaderComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        FormsModule,
-        HttpClientModule
-        // InMemoryWebApiModule.forRoot(InMemoryService, { delay: 1000 })
-    ],
-    providers: [AuthService, httpInterceptProviders],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
+    // InMemoryWebApiModule.forRoot(InMemoryService, { delay: 1000 })
+  ],
+  providers: [
+    AuthService,
+    LoaderService,
+    httpInterceptProviders],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
